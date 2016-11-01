e354038beb39e000680195ab22fc90cd4a2935d8
unknown <uma@umamadev3.redmond.corp.microsoft.com>
2016-10-31 22:31:19 -0700

    initial checkin for FTC software

---
+++ SW/ftc_app-master/Teleop_Test1/app/src/main/java/com/example/uma/teleop_test1/MainActivity_Test1.java
@@ -0,0 +1,13 @@
+package com.example.uma.teleop_test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_Test1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__test1);
+    }
+}
